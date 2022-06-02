13ddf87517f6aad9c90204d2a5952781c373ea61
Paolo Castro <paolo.castro@colorado.edu>
2022-06-01 18:59:29 -0600

    Add final pages

--- web/routes.py
+++ web/routes.py
@@ -43,3 +43,13 @@ def project4():
 @app.route("/project5")
 def project5():
     return flask.render_template("empty-project.html", nav_page="project5")
+
+
+@app.route("/statements")
+def statements():
+    return flask.render_template("statements.html", nav_page="statements")
+
+
+@app.route("/reflections")
+def reflections():
+    return flask.render_template("reflections.html", nav_page="reflections")

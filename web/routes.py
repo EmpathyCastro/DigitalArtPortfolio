ed0e90f1748d17dfb6745130dbfd1d47fb0fc5a6
Paolo Castro <paolo.castro@colorado.edu>
2022-06-01 18:33:18 -0600

    Some fixes

--- web/routes.py
+++ web/routes.py
@@ -18,3 +18,28 @@ def internal_server_error(_):
 @app.route("/")
 def home():
     return flask.render_template("home.html", nav_page="home", splash=True)
+
+
+@app.route("/project1")
+def project1():
+    return flask.render_template("empty-project.html", nav_page="project1")
+
+
+@app.route("/project2")
+def project2():
+    return flask.render_template("empty-project.html", nav_page="project2")
+
+
+@app.route("/project3")
+def project3():
+    return flask.render_template("empty-project.html", nav_page="project3")
+
+
+@app.route("/project4")
+def project4():
+    return flask.render_template("empty-project.html", nav_page="project4")
+
+
+@app.route("/project5")
+def project5():
+    return flask.render_template("empty-project.html", nav_page="project5")
